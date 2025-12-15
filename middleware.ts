23f6c76319c897f7167e65f01bfac52239802fb5
youshi-kanda <kanda02.1203@gmail.com>
2025-12-15 12:26:07 +0900

    chore: middlewareでセッション更新処理を無効化し、NextResponseを使用するように変更。

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { type NextRequest, NextResponse } from 'next/server'
+// import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-    return await updateSession(request)
+    // return await updateSession(request)
+    return NextResponse.next();
 }
 
 export const config = {
